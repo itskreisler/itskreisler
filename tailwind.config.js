d0742913e9f3cdb886ff0c6a9e584511bb189569
Kreisler Ramirez <itskreisler@outlook.com>
2026-04-14 17:08:00 -0500

    fix: ajustes en navegación y dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class', // Habilita el modo oscuro por clase
-  content: [
-    './src/**/*.{astro,js,jsx,ts,tsx}',
-    './public/**/*.html',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    darkMode: 'class', // Habilita el modo oscuro por clase
+    content: [
+        './src/**/*.{astro,js,jsx,ts,tsx}',
+        './public/**/*.html',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
